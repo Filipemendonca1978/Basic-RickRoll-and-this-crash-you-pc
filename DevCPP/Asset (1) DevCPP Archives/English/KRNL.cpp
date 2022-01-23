70f6bf645b2031169094722e46b400518abab65e
Filipe Daniel <82539588+Filipemendonca1978@users.noreply.github.com>
2022-01-22 22:50:18 -0300

    Add files via upload

---
+++ DevCPP/Asset (1) DevCPP Archives/English/KRNL.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <windows.h>
+
+
+int main(int argc, char** argv) {
+	
+	SetConsoleTitle("HAHA! (Coded by FilipeET)");
+
+HWND wnd = FindWindow(0, "HAHA! (Coded by FilipeET)");
+
+if( wnd != NULL )
+
+{
+
+ShowWindow(wnd, SW_HIDE);
+
+}
+
+	std::cout << "HAHAHA!\n";
+    system("@break off");
+    system("color 07");
+    Sleep (1000);
+    system("color 02");
+    Sleep (1000);
+    system("color 07");
+    Sleep (1000);
+    system("color 02");
+    Sleep (1000);
+    system("color 07");
+    Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 00");
+	Sleep (1000);
+	std::cout << "Nyan Cat Killed YOUR PC";
+    Sleep (1000);
+	Sleep (100000000);
+	system("pause");
+}
---
+++ DevCPP/Asset (1) DevCPP Archives/Portuguese (Original)/KRNL.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <windows.h>
+
+
+int main(int argc, char** argv) {
+	
+	SetConsoleTitle("Ja era porque este programa nunca fechara, Programado por FT");
+
+HWND wnd = FindWindow(0, "Ja era");
+
+if( wnd != NULL )
+
+{
+
+ShowWindow(wnd, SW_HIDE);
+
+}
+
+	std::cout << "HAHAHA Voce Nao escapa!\n";
+    system("@break off");
+    system("color 07");
+    Sleep (1000);
+    system("color 02");
+    Sleep (1000);
+    system("color 07");
+    Sleep (1000);
+    system("color 02");
+    Sleep (1000);
+    system("color 07");
+    Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 07");
+	Sleep (1000);
+	system("color 02");
+	Sleep (1000);
+	system("color 00");
+	Sleep (1000);
+	std::cout << "Ainda esta aqui?\n";
+    Sleep (1000);
+	Sleep (100000000);
+	system("pause");
+}
